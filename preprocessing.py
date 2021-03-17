9f5d837a8eea6f99f4b13bd90b2ef9ce9a25663c
Cecile Macaire <cecile.macaire@live.com>
2021-03-17 09:30:07 +0100

    data from na + update preprocessing script to print stats from data

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,4 @@
+import os
 import xml.etree.ElementTree as et
 from os import listdir
 from os.path import join, isfile
@@ -6,7 +7,9 @@ from pathlib import Path
 import csv
 import pandas as pd
 import argparse
-
+from pydub import AudioSegment
+import librosa
+from sklearn.utils import shuffle
 
 def get_files_from_directory(path):
     """
@@ -16,6 +19,23 @@ def get_files_from_directory(path):
     """
     files = [f for f in listdir(path) if isfile(join(path, f))]
     return files
+
+
+def convert_mp3towav(args):
+    input = args.path_input
+    output = args.path_output
+    files = get_files_from_directory(input)
+    for f in files:
+        sound = AudioSegment.from_mp3(input + f)
+        sound.export(output + f[:-4]+'.wav', format="wav")
+
+
+def statistics(args):
+    path = args.path+'process_wav/'
+    wav = get_files_from_directory(args.path+'process_wav/')
+    duration = [librosa.get_duration(filename=path+f) for f in wav]
+    print("Total duration of corpus in secondes: ", round(sum(duration), 2))
+    print("\nTotal duration of corpus in minutes: ", round(sum(duration), 2)/60)
 
 
 def extract_information(xml_file):
@@ -45,7 +65,7 @@ def create_audio_tsv(args):
     :param args: path
     """
     path = args.path
-    files = get_files_from_directory(path+'trans/')
+    files = get_files_from_directory(path + 'trans/')
 
     tsv = open(path + 'all.tsv', 'wt')
     tsv_writer = csv.writer(tsv, delimiter='\t')
@@ -61,7 +81,7 @@ def create_audio_tsv(args):
 
         for k, v in info.items():
             tfm = sox.Transformer()
-            tfm.trim(float(v[0])-0.2, float(v[1])+0.2)
+            tfm.trim(float(v[0]), float(v[1]) + 0.2)
             tfm.compand()
             output_wav = f[:-4] + '_' + v[2] + '.wav'
             tfm.build_file(wav_file, str(wav_dir) + '/' + output_wav)
@@ -78,7 +98,8 @@ def create_dataset(args):
     path = args.path
 
     corpus = pd.read_csv(path + 'all.tsv', sep='\t')
-    corpus.sample(frac=1)
+    corpus = shuffle(corpus)
+    # corpus.sample(frac=1)
 
     size_corpus = corpus.shape[0]
 
@@ -97,10 +118,25 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(help='sub-command help')
 
+    convert = subparsers.add_parser("convert",
+                                    help="Convert mp3 audio to wav format.")
+    convert.add_argument('--path_input', type=str, required=True,
+                         help="Path of the corpus with mp3 files.")
+    convert.add_argument('--path_output', type=str, required=True,
+                         help="Path to store the converted audio files.")
+    convert.set_defaults(func=convert_mp3towav)
+
+    stats = subparsers.add_parser("stats",
+                                    help="Some stats from the corpus.")
+    stats.add_argument('--path', type=str, required=True,
+                         help="Path of the corpus.")
+    stats.set_defaults(func=statistics)
+
     create_audio = subparsers.add_parser("create_audio",
                                          help="Create audio per sentences from xml and wav and store the info in a tsv file. "
                                               "Make sure the transcriptions are in /trans and wav in /wav")
-    create_audio.add_argument('--path', type=str, required=True, help="path of the corpus with wav and transcription files.")
+    create_audio.add_argument('--path', type=str, required=True,
+                              help="path of the corpus with wav and transcription files.")
     create_audio.set_defaults(func=create_audio_tsv)
 
     split_dataset = subparsers.add_parser("create_dataset",
